f69e7348736c5cf3e4d925e6a962d9a5b878a95f
dvir2008 <dvircaspi1@gmail.com>
2025-07-28 22:24:29 +0300

    dont know

--- stam.py
+++
@@ -1,8 +0,0 @@
-milim_nefotzot = ["12345678", "admin", "password", "11111111"]
-x = "12345678S"
-milim_nefotzot_t_o_f = False
-for i in range(len(milim_nefotzot)):
-    if x in milim_nefotzot[i]:
-        milim_nefotzot_t_o_f = True
-    break
-print(milim_nefotzot_t_o_f)
