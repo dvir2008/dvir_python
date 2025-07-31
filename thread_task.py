382300e11626ccd2e9d3e9c74be23b58c05af797
dvir2008 <dvircaspi1@gmail.com>
2025-07-31 12:49:40 +0300

    Theds mission mix

--- thread_task.py
+++ thread_task.py
@@ -1,16 +1,19 @@
-import threading
+from threading import Thread,Lock
 a = 0
+l = Lock()
 def thread_plos():
     global a
-    for i in range(100):
-        a+=1
+    for i in range(100000):
+        with l:
+            a+=1
 def thread_minos():
     global a
-    for i in range(100):
-        a-=1
+    for i in range(100000):
+        with l:    
+         a-=1
 
-thread1 = threading.Thread(target=thread_plos)
-thread2 = threading.Thread(target=thread_minos)
+thread1 = Thread(target=thread_plos)
+thread2 = Thread(target=thread_minos)
 
 thread1.start()
 thread2.start()
